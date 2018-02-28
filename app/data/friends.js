24265aff95ea054f3f148b26f8083dd098854117
Payam <payamasem@berkeley.edu>
2018-02-27 20:19:26 -0800

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,50 @@
 var friends = [ {
     "name" : "Donald",
     "photo" : "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg?v=1494613853",
-    "scores" : [1,1,1,1,1,1,1,1,1,1]
-}, {
+    "scores" : [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+}, 
+{
     "name" : "Hillary",
     "photo" : "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE4MDAzNDEwMDU4NTc3NDIy/hillary-clinton-9251306-2-402.jpg",
-    "scores" : [3,3,3,3,3,3,3,3,3,3]
-}, {
+    "scores" : [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+}, 
+{
     "name" : "Kim",
     "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Kim_Jong-Un_Photorealistic-Sketch.jpg/220px-Kim_Jong-Un_Photorealistic-Sketch.jpg",
-    "scores" : [5,5,5,5,5,5,5,5,5,5]
+    "scores" : [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
 }];
 
 module.exports = friends;
